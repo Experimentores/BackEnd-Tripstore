befe5395ff4a474b2932112df46ec4e4103ae661
DelfyNoLimits <nickhardav@gmail.com>
2023-09-23 21:26:16 -0500

    First Upload

--- src/main/java/com/upc/edu/BackEndTripStore/model/User.java
+++ src/main/java/com/upc/edu/BackEndTripStore/model/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
